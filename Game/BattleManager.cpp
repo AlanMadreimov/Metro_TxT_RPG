8de4a125be832fb16a8cd2eef657b163b7d8d8a0
Kirill <K0walski707@yandex.ru>
2025-05-12 22:06:28 +0300

    Created new folder Game and added game files to there

---
+++ Game/BattleManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by k0wal on 12.05.2025.
+//
+
+#include "BattleManager.h"
---
+++ Game/BattleManager.h
@@ -0,0 +1,16 @@
+//
+// Created by k0wal on 12.05.2025.
+//
+
+#ifndef BATTLEMANAGER_H
+#define BATTLEMANAGER_H
+
+
+
+class BattleManager {
+
+};
+
+
+
+#endif //BATTLEMANAGER_H
